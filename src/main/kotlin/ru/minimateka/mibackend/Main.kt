20476ae93be1b2c241d75705e6b66b5ee2a214b6
Alex Bro <48734513+AlexeyBro@users.noreply.github.com>
2024-06-28 09:39:39 +0200

    Update Main.kt

--- src/main/kotlin/ru/minimateka/mibackend/Main.kt
+++ src/main/kotlin/ru/minimateka/mibackend/Main.kt
@@ -10,7 +10,7 @@ import io.ktor.server.routing.*
 fun main() {
     embeddedServer(
         Netty,
-        port = 8082,
+        port = 8080,
         host = "0.0.0.0",
         module = Application::module
     ).start(wait = true)
